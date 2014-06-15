04ffdbd3c63a25f8cfe7f1331d46fe2db6a95b64
mbican <bicanm@gmail.com>
2014-06-15 10:30:34 +0200

    initial commit + create 2048Model interface

---
+++ 2048/2048Model.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+	class _2048Model
+	{
+		public bool left()
+		{
+			return this.move(_2048MoveDirection.left);
+		}
+
+		public bool right()
+		{
+			return this.move(_2048MoveDirection.right);
+		}
+
+		public bool up()
+		{
+			return this.move(_2048MoveDirection.up);
+		}
+
+		public bool down()
+		{
+			return this.move(_2048MoveDirection.down);
+		}
+
+		public bool move(_2048MoveDirection move)
+		{
+			switch (move)
+			{
+				case _2048MoveDirection.left:
+					return this.left();
+				case _2048MoveDirection.right:
+					return this.right();
+				case _2048MoveDirection.up:
+					return this.up();
+				case _2048MoveDirection.down:
+					return this.down();
+				default:
+					throw new ArgumentException("unknown move","move");
+			}
+		}
+
+	}
+
+}
---
+++ 2048/2048MoveDirection.cs
@@ -0,0 +1,10 @@
+﻿namespace _2048
+{
+	enum _2048MoveDirection
+	{
+		left,
+		right,
+		up,
+		down,
+	}
+}
