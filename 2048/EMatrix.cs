53dee6032284d6f0c4f8f484cba04acfe9dd2982
mbican <bicanm@gmail.com>
2014-06-16 19:48:14 +0200

    create IMatrix extension Rotate method.

--- 2048/EMatrix.cs
+++ 2048/EMatrix.cs
@@ -9,7 +9,7 @@ namespace _2048
 {
 	static class EMatrix
 	{
-		public static string ToDebugString(this Matrix<bool> matrix)
+		public static string ToDebugString(this IMatrix<bool> matrix)
 		{
 			var result=new StringBuilder();
 			for (var row = 0; row < matrix.RowCount; ++row)
@@ -26,6 +26,15 @@ namespace _2048
 				result.AppendLine();
 			}
 			return result.ToString();
+		}
+
+
+		public static IMatrix<T> Rotate<T>(this IMatrix<T> matrix)
+		{
+			if (matrix == null)
+			{
+				throw new ArgumentNullException("matrix");
+			}
 		}
 	}
 }
